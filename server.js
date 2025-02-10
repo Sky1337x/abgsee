4aa2a82887a565fdb1b3645c5ff956753a7827eb
Sky <122771939+Sky1337x@users.noreply.github.com>
2025-02-10 18:31:03 +0530

    ds

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const path = require('path');
-const mysql = require('mysql');
+
 const dotenv = require('dotenv');
 var favicon = require('serve-favicon')
 const app = express();
@@ -9,13 +9,6 @@ const port = 3000
 
 dotenv.config({ path:'./.env'})
 
-const db = mysql.createConnection({
-  host: process.env.DATABASE_HOST,
-  user: process.env.DATABASE_USER,
-  password: process.env.DATABASE_PASSWORD,
-  database: process.env.DATABASE
-});
-
 
 app.set('view engine','hbs');
 
@@ -23,14 +16,6 @@ app.set('view engine','hbs');
 const publicDirectory = path.join(__dirname,'./public')
 app.use(express.static(publicDirectory));
 
-db.connect((error) => {
-  if(error){
-    console.log(error)
-  }
-  else{
-    console.log("sql connected")
-  }
-});
 
 app.get('/', (req, res) => {
     res.render("index")
